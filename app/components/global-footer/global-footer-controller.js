1924d70ceb166f8bdec7f9aeb952745e661a86ba
Michael Sholty <michael.sholty@fanduel.com>
2016-05-21 11:18:34 -0400

    feat(footer): add and style social buttons

--- app/components/global-footer/global-footer-controller.js
+++ app/components/global-footer/global-footer-controller.js
@@ -1,5 +1,24 @@
 export default class GlobalFooterController {
-    // constructor() {
+    constructor($window) {
+        this.$window = $window;
 
-    // }
+        this.socialButtons = [
+            {
+                type: 'twitter',
+                url: 'www.twitter.com/dnlperformance'
+            },
+            {
+                type: 'facebook',
+                url: 'www.twitter.com/dnlperformance'
+            },
+            {
+                type: 'gplus',
+                url: 'www.twitter.com/dnlperformance'
+            }
+        ];
+    }
+
+    redirect(url) {
+        this.$window.location.href = url;
+    }
 }
