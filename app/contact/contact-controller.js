93fabf8343dfab571a864ab4b41b27b747a3749c
Michael Sholty <michael.sholty@fanduel.com>
2016-05-22 20:04:34 -0400

    feat(contact): issue api call to google to get place details

--- app/contact/contact-controller.js
+++ app/contact/contact-controller.js
@@ -1,7 +1,19 @@
-import _ from 'lodash';
-
 export default class ContactController {
-    constructor() {
-        this.availableYears = _.range(1995, 2017).reverse();
+    constructor(apiKey, $sce, DNLGApi) {
+        this.mapUrl = `https://www.google.com/maps/embed/v1/place?key=${apiKey}
+        &q=D%26L+Performance+Gainesville+Subaru+Specialist's`;
+
+        this.apiKey = apiKey;
+        this.$sce = $sce;
+        this.DNLGApi = DNLGApi;
+        //this.PLACES_API_PATH = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=${apiKey}`;
+
+        this.DNLGApi.getPlaceDetails().then((response) => {
+            this.placeDetails = response.data.result;
+        });
+    }
+
+    trustSrc(src) {
+        return this.$sce.trustAsResourceUrl(src);
     }
 }
