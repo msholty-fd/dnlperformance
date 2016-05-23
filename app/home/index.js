177260fd711f34ca1b692b354397f4571a5d9e20
Michael Sholty <michael.sholty@fanduel.com>
2016-05-22 20:01:36 -0400

    refactor(home): add testimonials and scheduler components

--- app/home/index.js
+++ app/home/index.js
@@ -1,10 +1,16 @@
 import angular from 'angular';
-import HomeController from './home-controller';
-import template from './home.html';
 import 'angular-ui-router';
 
+import '../components/appointment-scheduler';
+import '../components/testimonials';
+
+import HomeController from './home-controller';
+import template from './home.html';
+
 export default angular.module('dnl.home', [
-    'ui.router'
+    'ui.router',
+    'dnl.components.appointment-scheduler',
+    'dnl.components.testimonials'
 ])
 
 .config( /* @ngInject */ function($urlRouterProvider, $stateProvider) {
