5e4fde17e6f32bf0061b90e1311165601a363e91
Michael Sholty <michael.sholty@fanduel.com>
2016-07-06 14:02:26 -0400

    feat: odds and ends

--- app/home/home-controller.js
+++ app/home/home-controller.js
@@ -1,1 +1,16 @@
-export default class HomeController {}
+export default class HomeController {
+    constructor() {
+        this.myInterval = 5000;
+        this.noWrapSlides = false;
+        this.active = 0;
+        this.carouselSlides = [{
+            id: 1,
+            image: 'logo/1.jpg',
+            text: 'Your local Gainesville Subaru specialist'
+        }, {
+            id: 2,
+            image: 'logo/2.jpg',
+            text: 'Another image'
+        }];
+    }
+}
