2a7be8b3d58a2f05dec4a21d0393ecb5835927e1
Michael Sholty <michael.sholty@fanduel.com>
2016-08-15 17:00:58 -0400

    refactor: update config.js

--- gulp/config.js
+++ gulp/config.js
@@ -1,12 +1,8 @@
 const config = {};
 
 config.UI_S3_URL = '//s3.amazonaws.com/';
-config.UI_CLOUDFRONT_URL = '//dt80l7nkb7hnz.cloudfront.net/';
-config.STYLEGUIDE_VERSION = '5.2.1';
-config.STYLEGUIDE_TEST_URL = '//d19uw7vtmf504j.cloudfront.net/';
-config.STYLEGUIDE_CLOUDFRONT_URL = '//d2avoc1xjbdrch.cloudfront.net/';
-config.STYLEGUIDE_BASE = getStyleguideBase();
-config.STYLEGUIDE_LOCAL_PORT = 9001;
+config.UI_CLOUDFRONT_URL = '//d9fsetxygehvw.cloudfront.net/';
+config.WAM_LOCAL_PORT = 3000;
 
 config.LIVERELOAD_PORT = 35727;
 config.LIVERELOAD_URL = `https://localhost:${config.LIVERELOAD_PORT}/livereload.js?snipver=1`;
@@ -29,13 +25,5 @@ config.src = {
 		'./app/assets/**/*'
 	]
 };
-
-function getStyleguideBase() {
-	if (process.env.STYLEGUIDE_BRANCH && !process.env.PRODUCTION) {
-		return `${config.STYLEGUIDE_TEST_URL}${process.env.STYLEGUIDE_BRANCH}/`;
-	}
-
-	return `${config.STYLEGUIDE_CLOUDFRONT_URL}${config.STYLEGUIDE_VERSION}/`;
-}
 
 export default config;
