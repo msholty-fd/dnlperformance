6296610f386841875d8de5df153035fdbebee59b
Michael Sholty <michael.sholty@fanduel.com>
2016-09-22 21:53:10 -0400

    feat: add global header drawer component

--- app/components/global-header/index.js
+++ app/components/global-header/index.js
@@ -2,7 +2,11 @@ import angular from 'angular';
 import template from './global-header.html';
 import GlobalHeaderController from './global-header-controller';
 
-export default angular.module('dnl.components.global-header', [])
+import globalHeaderDrawerModule from './global-header-drawer.js';
+
+export default angular.module('dnl.components.global-header', [
+    globalHeaderDrawerModule.name
+])
 
 .component('globalHeader', {
     controllerAs: 'globalHeader',
